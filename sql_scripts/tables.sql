937db7ea2c5fe74ac48afe9ed6f875630a40c31e
RobertBSalmon <rsalmon2@uncc.edu>
2023-03-15 17:07:37 -0400

    Initial commit

---
+++ sql_scripts/tables.sql
@@ -0,0 +1,15 @@
+-- resources Table:
+CREATE TABLE IF NOT EXISTS resources (
+    id int NOT NULL AUTO_INCREMENT,
+    item varchar(50) NOT NULL,
+    amount int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- sandwiches table:
+CREATE TABLE IF NOT EXISTS sandwich (
+    id int NOT NULL AUTO_INCREMENT,
+    sandwich_size varchar(50) NOT NULL,
+    price decimal(5,2)  NOT NULL,
+    PRIMARY KEY (id)
+);
